048e24e792d554f621e76fd07918338ace861744
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-12 21:18:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,56 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+require 'pry'
+def roll_call_dwarves(list)
+  
+  list.each_with_index do |dwarf,index| 
+    
+    num = index + 1
+    puts "#{num}  #{dwarf}"
+    
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  
+  
+  arr.map!{ |element| element.capitalize + "!"}
+    
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+ 
+  arr.each do |word|
+   
+    if word.length > 4
+     
+      return true
+   
+    end
+ 
+  end
+
+  false
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(snacks)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.each do |cheese|
+    
+    if snacks.include?(cheese) == true
+      
+      return cheese
+      
+    end
+    
+  end
+  
+  nil
+  
 end

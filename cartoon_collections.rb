72e593e51f80341dcfb0ecdf94fb47b2ac39835f
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-12 21:11:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -43,7 +43,7 @@ def find_the_cheese(snacks)
   
   cheese_types.each do |cheese|
     
-    if snacks.include?(cheese) == true
+    if snacks.include?(cheese) 
       
       return true
       
@@ -51,6 +51,6 @@ def find_the_cheese(snacks)
     
   end
   
-  false
+  
   
 end
